1bc048c8aa0bf29bdc50d82dea9eb395554d6d71
Leonardo Cavalcante <113479204+nominalism@users.noreply.github.com>
2023-05-08 20:07:50 -0300

    Update extra 2.py
    another translation

--- extra 2.py
+++ extra 2.py
@@ -1,28 +1,27 @@
-def convertedecimalparabase(numdec, base, simbolos):
-    numeroconvertido = ""
+def convertedecimaltobase(numdec, base, symbols):
+    convertednumber = ""
     while numdec > 0:
         d = numdec % base
-        digit = simbolos[d]
-        numeroconvertido = digit + numeroconvertido
+        digit = symbols[d]
+        convertednumber = digit + convertednumber
         numdec = int(numdec / base)
-    return numeroconvertido
+    return convertednumber
 
 
-
-numdec = int(input(" Número a ser convertido: "))
-print("Opções de conversão: (1) para Octal; (2) para Binário; (3) para Hexadecimal.")
-base = int(input(' Escolha a base de destino: '))
+numdec = int(input("Number to be converted: "))
+print("Conversion options: (1) to Octal; (2) to Binary; (3) to Hexadecimal.")
+base = int(input('Choose the target base: '))
 if base == 1:
-    simbolos = ('0', '1', '2', '3', '4', '5', '6', '7')
-    result = convertedecimalparabase(numdec, 8, simbolos)
+    symbols = ('0', '1', '2', '3', '4', '5', '6', '7')
+    result = convertedecimaltobase(numdec, 8, symbols)
     print(result)
 elif base == 2:
-    simbolos = ('0', '1')
-    result = convertedecimalparabase(numdec, 2, simbolos)
+    symbols = ('0', '1')
+    result = convertedecimaltobase(numdec, 2, symbols)
     print(result)
 elif base == 3:
-    simbolos = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F')
-    result = convertedecimalparabase(numdec, 16, simbolos)
+    symbols = ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F')
+    result = convertedecimaltobase(numdec, 16, symbols)
     print(result)
 else:
-    print(' Opção inválida! ')
+    print('Invalid option!') 
