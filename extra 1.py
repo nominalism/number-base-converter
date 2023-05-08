feb91d1249096fe4b2a2f027a828706af287b6a0
Leonardo Cavalcante <113479204+nominalism@users.noreply.github.com>
2023-05-08 20:06:12 -0300

    Update extra 1.py
    another translation

--- extra 1.py
+++ extra 1.py
@@ -1,61 +1,54 @@
-from re import A
-
-
-def main(n, opcao):
-        if opcao == 1:
-            numeroOctal = ""
-            while n > 0:
-                d = n % 8
-                digit = str(d)
-                numeroOctal = digit + numeroOctal
-                n = int(float(n) / 8)
-            if len(numeroOctal) == 0:
-                numeroOctal = "0"
-            print(numeroOctal)
+def main(n, option):
+    if option == 1:
+        octal_number = ""
+        while n > 0:
+            d = n % 8
+            digit = str(d)
+            octal_number = digit + octal_number
+            n = int(float(n) / 8)
+        if len(octal_number) == 0:
+            octal_number = "0"
+        print(octal_number)
         
+    elif option == 2:
+        binary_number = ""
+        while n > 0:
+            d = n % 2
+            digit = str(d)
+            binary_number = digit + binary_number
+            n = int(float(n) / 2)
+        if len(binary_number) == 0:
+            binary_number = "0"
+        print(binary_number)
         
-        elif opcao == 2:
-                numerobin = ""
-                while n > 0:
-                    d = n % 2
-                    digit = str(d)
-                    numerobin = digit + numerobin
-                    n = int(float(n) / 2)
-                if len(numerobin) == 0:
-                    numerobin = "0"
-                print(numerobin)
-        
-        
-        elif opcao == 3:
-                numeroexa = ""
-                while n > 0:
-                    d = n % 16
-                    digit = str(d)
+    elif option == 3:
+        hexadecimal_number = ""
+        while n > 0:
+            d = n % 16
+            digit = str(d)
 
-                    if d >= 10:
-                        if d == 10:
-                            digit = 'A'
-                        if d == 11:
-                            digit = 'B'
-                        if d == 12:
-                            digit = 'C'
-                        if d == 13:
-                            digit = 'D'
-                        if d == 14:
-                            digit = 'E'
-                        if d == 15:
-                            digit = 'F' 
-                    numeroexa = digit + numeroexa
-                    n = int(float(n) / 16)
-                if len(numeroexa) == 0:
-                    numeroexa = "0"
-                print(numeroexa)
-        else:
-            print("Opção inválida!")
+            if d >= 10:
+                if d == 10:
+                    digit = 'A'
+                if d == 11:
+                    digit = 'B'
+                if d == 12:
+                    digit = 'C'
+                if d == 13:
+                    digit = 'D'
+                if d == 14:
+                    digit = 'E'
+                if d == 15:
+                    digit = 'F' 
+            hexadecimal_number = digit + hexadecimal_number
+            n = int(float(n) / 16)
+        if len(hexadecimal_number) == 0:
+            hexadecimal_number = "0"
+        print(hexadecimal_number)
+    else:
+        print("Invalid option!")
 
-
-
-n = int(input(" Número a ser convertido: "))
-print("Opções de conversão: (1) para Octal; (2) para Binário; (3) para Hexadecimal.")
-opcao = int(input())
-main(n, opcao)
+n = int(input("Number to convert: "))
+print("Conversion options: (1) to Octal; (2) to Binary; (3) to Hexadecimal.")
+option = int(input())
+main(n, option)
